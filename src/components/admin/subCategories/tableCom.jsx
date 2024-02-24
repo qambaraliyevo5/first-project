57b1bec56defc63cf76b5adaaa3b24e8c8192a99
55zakii <zodilbek2009@gmail.com>
2024-02-24 17:00:10 +0500

    odilbek

--- src/components/admin/subCategories/tableCom.jsx
+++ src/components/admin/subCategories/tableCom.jsx
@@ -65,7 +65,6 @@ const TableCom = () => {
         </table>
       </div>
       <table class="w-full  overflow-y-scroll  text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        
       </table>
     </div>
   );
